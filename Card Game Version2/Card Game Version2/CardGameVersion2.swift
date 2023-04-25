802ada2cd2c947ac2058797a9aa1148f569559ff
Robert <47223029+robertnochez@users.noreply.github.com>
2023-04-24 23:25:09 -0700

    Add Files Via Upload
    Demo: https://drive.google.com/file/d/10DoPKy3lxsLv9AwYRUnR-fh3W5brbqEV/view?usp=sharing

---
+++ Card Game Version2/Card Game Version2/CardGameVersion2.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameVersion2App.swift
+//  CardGameVersion2
+//
+//  Created by Robert Nochez on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CardGameVersion2A: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
